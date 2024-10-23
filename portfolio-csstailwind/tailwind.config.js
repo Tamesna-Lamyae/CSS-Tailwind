d4400037265efbb090bd7d7557612b39004b21f6
Tamesna-Lamyae <lamyae.tamesna@uit.ac.ma>
2024-10-23 14:03:50 +0100

    first commit

---
+++ portfolio-csstailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+document.getElementById('sendButton').addEventListener('click', function() {
+  alert('Button clicked!');
+});
